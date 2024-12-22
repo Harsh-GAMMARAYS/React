4f72196816480877f462cb2e8ab69d20aa585227
Harsh-GAMMARAYS <gammaray.coding@gmail.com>
2024-12-22 23:49:33 +0530

    week8

---
+++ week8/week8-2/src/stories/RevenueCard.stories.jsx
@@ -0,0 +1,23 @@
+import { RevenueCard } from "../components/RevenueCard";
+
+const meta = {
+    component: RevenueCard,
+};
+
+export default meta;
+
+export const Primary = {
+    args: {
+        orderCount:"12345",
+        title: "Button Big",
+        amount: "1000"
+    },
+};
+
+export const Secondary = {
+    args: {
+        orderCount:"123",
+        title: "Button",
+        amount: "10"
+    },
+};
