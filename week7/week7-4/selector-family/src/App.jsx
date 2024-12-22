2d96d3ea0b994b23502cdafcf1584907cbbcbcb7
Harsh-GAMMARAYS <gammaray.coding@gmail.com>
2024-12-22 16:35:04 +0530

    selector-family

--- week7/week7-4/selector-family/src/App.jsx
+++ week7/week7-4/selector-family/src/App.jsx
@@ -1,25 +1,39 @@
 
 import './App.css'
-import { RecoilRoot, useRecoilState } from 'recoil';
+import { Suspense } from 'react';
+import { RecoilRoot, useRecoilState, useRecoilValue } from 'recoil';
 import { todosAtomFamily } from './atoms';
 
 function App() {
-  return <RecoilRoot>
-    <Todo id={1}/>
-    <Todo id={2} />
-  </RecoilRoot>
+  const todoIds = Array.from({ length: 200 }, (_, i) => i + 1);
+
+  return (
+    <RecoilRoot>
+      <Suspense fallback={<div>Loading...</div>}>
+        {todoIds.map((id, index) => (
+          <div key={id}>
+            <Todo id={id} />
+            {index !== todoIds.length - 1 && <br />}
+          </div>
+        ))}
+      </Suspense>
+    </RecoilRoot>
+  );
 }
 
 function Todo({id}) {
-   const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
+  const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
+  
+  if (!todo) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
-      {todo.title}
-      {todo.description}
+      {todo.title} {todo.completed ? "✅" : "❌"}
       <br />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
